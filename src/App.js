9a4a8687c1b64a89ae19d69dda55b79b5ac5eef2
Weronika Tomaszewska <vikariusu@gmail.com>
2017-09-19 19:50:56 +0200

    convert markup to output

--- src/App.js
+++ src/App.js
@@ -2,13 +2,29 @@ import React, { Component } from 'react';
 import './css/App.css';
 import marked from 'marked';
 
-
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { text: "" };
+  }
+
+  handleChange = () => {
+    if (document.getElementsByClassName('text-field')[0]) {
+      const enteredText = document.getElementsByClassName('text-field')[0].value;
+      this.setState({text: enteredText});
+    }
+  }
+
+  createMarkup = () => {
+    const markup = marked(this.state.text);
+    return { '__html': markup };
+  }
+
   render() {
     return (
       <div>
-        <textarea></textarea>
-        <div>{marked('### I am using __markdown__.')}</div>
+        <textarea className="text-field" onChange={this.handleChange}></textarea>
+        <div dangerouslySetInnerHTML={this.createMarkup()}></div>
       </div>
     );
   }
